09a207d6c4713fb66e08b5242dc85cff46ae2cab
Michael Ramirez <mram7056@arecibointer.edu>
2024-03-06 11:25:27 -0400

    update file

---
+++ labs c++/ejemplo1.cs
@@ -0,0 +1,12 @@
+
+using System;
+class Program {
+    static void Main() {
+        int myVariable = 10;
+        const int myConstant = 20;
+        Console.WriteLine("Valor de mi variable: "
+        + myVariable);
+        10Console.WriteLine("Valor de mi constante: "
+        + myConstant);
+        }
+        }
