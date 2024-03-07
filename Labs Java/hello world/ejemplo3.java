b838bcb2b9c6e71e41fdc0a165f5f359ddee2589
Michael Ramirez <mram7056@arecibointer.edu>
2024-03-07 09:24:31 -0400

    xd

--- Labs Java/hello world/ejemplo3.java
+++ Labs Java/hello world/ejemplo3.java
@@ -1,11 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
+public class ejemplo3
+{
+    public static void main(String[] args) 
+    {
         int myVariable = 10;
         final int myConstant = 20;
 
-        System.out.println("Valor de mi variable: "
-        + myVariable);
-        System.out.println("Valor de mi constante: "
-        + myConstant);
+        System.out.println("Valor de mi variable:" + myVariable);
+
+        System.out.println("Valor de mi constante:" + myConstant);
     }
 }
