a09d0959edba822ff1b37c200b6dea5ac48e2bd4
Michael Ramirez <mram7056@arecibointer.edu>
2024-03-28 23:49:14 -0400

    update file

--- laboratorios c#/Mod4.cs
+++ laboratorios c#/Mod4.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program {
     static void Main() {
+        Console.Write("Ingrese si su tarjeta esta activa o inactiva : ");
+        int tarjeta = int.Parse(Console.ReadLine());
 
+        if(tarjeta == activa);
+        console.writeline("Se puede hacer prestamos porque su tarjeta esta activa.");
 
-
+        if(tarjeta == inactiva);
+        console.writeline("No se puede hacer prestamos porque su tarjeta esta inactiva.");
     }
 }
