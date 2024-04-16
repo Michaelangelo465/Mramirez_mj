61b06f9c4c4292648174ff614ac54c64f139d0c7
Michael Ramirez <mram7056@arecibointer.edu>
2024-04-15 21:49:37 -0400

    new file

---
+++ laboratorios c#/Act_Ciclo_Mod5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MOD5Act1Cicloprob2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Entre su nombre: ");
+            string nombre = Console.ReadLine();
+            
+            for (int i=0; i<11; i++) {
+                Console.WriteLine("Hola" + nombre + "!");
+
+            }
+                
+
+
+        }
+    }
+}
